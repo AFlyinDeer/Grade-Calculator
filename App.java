9214ce108ddda40b4c93c95c42fc84ffc9bc18e2
Wade Randel <waderandel6@gmail.com>
2022-12-13 15:03:49 -0700

    Completed FileReader class, started work on App.java, to-do GradeData.java

--- App.java
+++ App.java
@@ -1,3 +1,30 @@
-public class Main {
-    
+public class App {
+
+    public static void main(String[] args) {
+        
+        System.out.print("Enter the fully qualified path of the file you would like to access:\n");
+        String path = System.console().readLine();
+
+        String[] splitRawData = FileReader.readFileFromPath(path);
+        int[] grades = ConvertStringArrayToIntArray(splitRawData);
+
+        
+        System.out.println(grades.length);
+    }
+    private static int[] ConvertStringArrayToIntArray(String[] strArray){
+        
+        int[] intArray = new int[strArray.length];
+        for(int i = 0;i < strArray.length;i++)        
+        {
+            intArray[i] = Integer.parseInt(strArray[i]);
+        
+        }
+        return intArray;
+    }
 }
+
+
+
+
+
+// convert splitrawdata into int array
--- FileReader.java
+++ FileReader.java
@@ -1,3 +1,30 @@
-public class Main {
-    
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class FileReader {
+  public static String[] readFileFromPath (String path) {
+    try {
+      File file = new File("C:\\Users\\Darth Wader\\Desktop\\School\\Semester 4\\Java\\Projects\\Final\\grades_1H1.txt");
+      Scanner scanner = new Scanner(file);
+      String allLines = "";
+      while (scanner.hasNextLine()) {
+        String data = scanner.nextLine();
+        allLines = allLines + data + ",";
+        
+      }
+      allLines = allLines.substring(0, allLines.length() - 1);
+      String[] stringNumbers = allLines.split(",");
+      
+      scanner.close();
+      return stringNumbers;
+
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+    String[] results = {"a","b","c"};
+    return results;
+  }
 }
